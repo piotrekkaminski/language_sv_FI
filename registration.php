075d8487532feccdc36a8eaf8647b36b5ea46ed0
Piotr Kaminski <magentopeter@gmail.com>
2017-08-25 15:04:24 -0500

    Translation autoupdate from Crowdin for sv_FI - 2017-08-25 20:04:23 branch Head

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'magentotwotranslations_sv_fi',
+    __DIR__
+);
